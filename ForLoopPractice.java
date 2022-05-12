332c57b4951a7d439aaaa39f523c7dd8fe2830ad
panamabilly <billsweeney85@gmail.com>
2022-05-12 01:48:38 -0500

    For Loop Practice Reverse String

---
+++ ForLoopPractice.java
@@ -0,0 +1,22 @@
+package practice.loops;
+
+public class ForLoopPractice {
+	
+//	Code Challenge: Reverse the given string "abcdefghijklmnopqrstuvwxyz" and print it in the console.	
+	
+	public static void main(String args[]) {
+//		Given String
+		String name = "abcdefghijklmnopqrstuvwxyz";
+		
+//		 Strings are immutable (you can't change the order so you have to make it a array which is immutable. )Put string into an array so we have the ability to have an index for each char.
+		char stringarray[] = name.toCharArray();
+//		 Print array in a line by itself
+		System.out.println(stringarray);
+		
+		for (int i = stringarray.length -1; i >= 0; i--) {
+			System.out.print(stringarray[i]);
+		} 
+		
+	}
+
+}
